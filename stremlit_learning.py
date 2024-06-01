530de97db55ff801338394b161e1cddf4ef1aa8c
Shubham <shubham.mishra@nunam.com>
2024-06-01 12:43:39 +0530

    corrected unit display mistake

--- stremlit_learning.py
+++ stremlit_learning.py
@@ -7,4 +7,4 @@ st.subheader("Battery Energy Calculation")
 capacity = st.number_input("Enter Battery Capacity (Ah)", value=0.0)
 nom_v = st.number_input("Enter Nominal voltage (V)", value=0.0)
 power = capacity*nom_v/1000
-st.write(f"Total Energy of the Battery is: {power}W")
+st.write(f"Total Energy of the Battery is: {power} kWh")

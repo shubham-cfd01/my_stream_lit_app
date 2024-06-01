53d5eb265f12a2585aa8ddcd1ea3323f11ec0e90
Shubham <shubham.mishra@nunam.com>
2024-06-01 12:41:33 +0530

    added energy calculation

--- stremlit_learning.py
+++ stremlit_learning.py
@@ -3,22 +3,8 @@ import streamlit as st
 st.title("Battery Calculator")
 
 # User inputs
-number1 = st.number_input("Enter first number", value=0.0)
-number2 = st.number_input("Enter second number", value=0.0)
-
-operation = st.selectbox("Choose an operation", ("Add", "Subtract", "Multiply", "Divide"))
-
-# Perform calculation
-if operation == "Add":
-    result = number1 + number2
-elif operation == "Subtract":
-    result = number1 - number2
-elif operation == "Multiply":
-    result = number1 * number2
-elif operation == "Divide":
-    if number2 != 0:
-        result = number1 / number2
-    else:
-        result = "Error: Division by zero"
-
-st.write(f"Result: {result}")
+st.subheader("Battery Energy Calculation")
+capacity = st.number_input("Enter Battery Capacity (Ah)", value=0.0)
+nom_v = st.number_input("Enter Nominal voltage (V)", value=0.0)
+power = capacity*nom_v/1000
+st.write(f"Total Energy of the Battery is: {power}W")
